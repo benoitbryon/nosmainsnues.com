5e582cfd09fd219126f6ce367f45a3d60870aa2e
Benoît Bryon <benoit@marmelune.net>
2016-11-29 09:56:41 +0100

    Prototyped nosmainsnues: digital facilitation and web development.

---
+++ README.md
@@ -0,0 +1,9 @@
+# L'élabomobile
+
+This is an early prototype for elabomobile.com website.
+
+* Requires npm and git
+* Clone the repository: git clone git@github.com:benoitbryon/elabomobile.git
+* Go to repository folder: cd elabomobile
+* Generate public files: make public
+* Open public/index.html: firefox public/index.html
---
+++ gorun_settings.py
@@ -0,0 +1,13 @@
+DIRECTORIES = (
+    ('assets/css', 'make css'),
+    ('assets/html', 'make html'),
+    ('assets/fonts', 'make fonts'),
+    ('assets/img', 'make img'),
+    ('assets/js', 'make jsmain'),
+    ('bower.json', 'make develop'),
+    ('bower_components', 'make css fonts jslibs'),
+    ('content', 'make html'),
+    ('Gemfile', 'make develop'),
+    ('package.json', 'make develop'),
+    ('requirements.pip', 'make develop'),
+)
